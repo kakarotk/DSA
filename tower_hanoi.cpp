3fad410b4cb6071481bf82cdb857de0b2b99ecf3
kakarotk <146341850+kakarotk@users.noreply.github.com>
2023-09-28 17:00:52 +0530

    3 c

---
+++ tower_hanoi.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+void move(int n, char source, char temp, char destination){
+    if(n==1){
+        cout<<"\n Move from "<<source<<" to "<<destination;
+    }
+    else{
+        move(n-1,source,destination,temp);
+        move(1,source,temp,destination);
+        move(n-1,temp,source,destination);
+    }
+}
+int main(){
+    int n;
+    cout<<"Enter number of disks: ";
+    cin>>n;
+
+    move(n,'A','B','C');
+    return 0;
+}
