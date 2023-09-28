90a0e527e7d88d71e4b97ab0d71c2a41ec20acd0
kakarotk <146341850+kakarotk@users.noreply.github.com>
2023-09-28 17:17:07 +0530

    5a

---
+++ bubblesort.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {7, 2, 3, 6, 9};
+    int N = sizeof(arr) / sizeof(arr[0]);
+
+    bubbleSort(arr, N);
+
+    cout << "Sorted array: \n";
+    printArray(arr, N);
+
+    return 0;
+}
